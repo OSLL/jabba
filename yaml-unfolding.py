66645b083040fef5906eaad69bcb60bdc1dfdc9f
missingdays <rebovykin@gmail.com>
2017-02-28 21:30:26 +0300

    Call graph. Job calls from #4. Test version

--- yaml-unfolding.py
+++ yaml-unfolding.py
@@ -4,6 +4,7 @@ from yaml import load, Loader, dump
 from argparse import ArgumentParser
 import graphviz as gv
 from os.path import basename
+import os
 
 from IncludeGraph import IncludeGraph
 
@@ -30,6 +31,12 @@ def include_raw_constructor(loader, node):
     return text
 
 def unfold_yaml(file_name):
+    '''
+    Unfolds file by given name
+
+    Also adds all included files as nodes to include graph. To disable this,
+    set include_graph.active = False before calling
+    '''
     include_graph.add_node(file_name)
     include_graph.add_to_list(file_name)
 
@@ -41,6 +48,60 @@ def unfold_yaml(file_name):
         include_graph.add_edge_from_last_node(file_name, 
                 label='<<B>include</B>>', color='include_color')
     return initial_dict
+
+def get_calls(file_name):
+    '''
+    Reads file by given name and returns array of tuples in form
+    (job_name, job_yaml) for all calls
+    '''
+    
+    file_dict = unfold_yaml(file_name)
+
+    return get_calls_from_dict(file_dict[0])
+
+def get_yaml_from_name(name):
+    '''
+    Finds .yaml config by given name
+    Slow version that will scan all files in the directory for each call
+    '''
+
+    cwd = os.getcwd()
+
+    # FIXME: Because we run script in root project, we also need to specify directory where all
+    # configs are. Do we need another command line parameter for that, or can we infer it somehow?
+    for filename in os.listdir(cwd + "/maxscale_jobs"):
+        if filename.endswith(".yaml"):
+            file_yaml = unfold_yaml("maxscale_jobs/" + filename)
+            file_yaml = file_yaml[0]
+
+            if 'job' in file_yaml:
+                if file_yaml['job']['name'] == name:
+                    return file_yaml
+
+    return None
+
+def extract_call(call):
+    call = call[0]
+    project = call['project']
+    file_yaml = get_yaml_from_name(project)
+    return (call['project'], file_yaml)
+
+def get_calls_from_dict(file_dict):
+
+    calls = []
+
+    if type(file_dict) == dict:
+        for key in file_dict:
+            if key == 'trigger-builds':
+                calls.append(extract_call(file_dict['trigger-builds']))
+            else:
+                calls.extend(get_calls_from_dict(file_dict[key]))
+    elif type(file_dict) == list:
+        for value in file_dict:
+            calls.extend(get_calls_from_dict(value))
+
+    return calls
+    
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -57,6 +118,8 @@ if __name__ == '__main__':
     Loader.add_constructor('!include', include_constructor)
     unfolded_yaml = unfold_yaml(args.file)
     #print(dump(unfolded_yaml, default_flow_style=False))
+
+    print(get_calls(args.file))
 
     if include_graph.active: 
         export_name = basename(args.file) + '_include'
