b97b51af7b7e11d8b1e0a82899bb521559ba464b
missingdays <rebovykin@gmail.com>
2017-03-14 19:33:41 +0300

    Add integration tests. Expand test output
    /scripts/run_tests.sh return exit code 1 if tests fail

---
+++ scripts/download_maxscale_jobs.sh
@@ -0,0 +1,12 @@
+if [ ! -e "/tmp/maxscale-jenkins-jobs" ]; then
+    echo -e "\e[92mCloning to /tmp/maxscale-jenkins-jobs\e[0m"
+    cwd=$(pwd)
+
+    {
+        git clone git@github.com:mariadb-corporation/maxscale-jenkins-jobs.git /tmp/maxscale-jenkins-jobs
+        cd /tmp/maxscale-jenkins-jobs/
+        git checkout 2d47218a68899c814d0e83c181e239ba73f3dd7d
+    } &> /dev/null
+
+    cd $cwd
+fi
--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,4 +1,4 @@
-echo "Running unit tests"
+echo -e "\e[93mRunning unit tests\e[0m"
 cd job_visualization
 python -m unittest discover --verbose
 cd .. 
