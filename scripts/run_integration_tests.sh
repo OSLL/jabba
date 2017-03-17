49a3620611fa1826947e75670f2aace7cf2aaa0a
missingdays <rebovykin@gmail.com>
2017-03-17 12:43:56 +0300

    Make integration tests output better

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -4,11 +4,24 @@ echo -e "\e[93mRunning integration tests\e[0m"
 
 cwd=$(pwd)/
 
-cat "integration_tests_data/test_commands.txt" | while read command
+exit_code=0
+total_OK=0
+total_ER=0
+
+while read command
 do 
     cd /tmp/maxscale-jenkins-jobs
     echo running ${command}
-    if !(eval "${cwd}${command}"); then echo error at ${command}; exit 1; fi
-    cd $cwd
-done
 
+    if !(eval "${cwd}${command}")
+    then echo error; exit_code=1; total_ER=$(($total_ER+1))
+    else total_OK=$(($total_OK+1)); echo "OK"; fi
+
+    cd $cwd
+done < <(cat "integration_tests_data/test_commands.txt")
+
+echo -e "\e[92mAll tests done\e[0m"
+echo $(($total_ER + $total_OK))" tests run"
+echo $total_ER" errors"
+
+exit $exit_code
