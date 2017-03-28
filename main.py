3d9a2c47b35d16c2994c9f91625e4ff67f48351e
missingdays <rebovykin@gmail.com>
2017-03-28 19:09:47 +0300

    Add comand line parameters help

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import argparse
 from argparse import ArgumentParser
+
 import os
 from os.path import basename
 
@@ -11,18 +13,19 @@ from job_visualization import synonym_parser
 
 if __name__ == '__main__':
 
-    parser = ArgumentParser()
-    parser.add_argument('--files', nargs='+', required=True, type=str)
-    parser.add_argument('--include-graph', dest='include_graph', action='store_true')
-    parser.add_argument('--call-graph', dest='call_graph', action='store_true')
-    parser.add_argument('--yaml-root', default='')
-    parser.add_argument('--call-display', choices=['none', 'text', 'edge'], default='text')
-    parser.add_argument('--rank-dir', choices=['left-right', 'up-down'], default='left-right')
-    parser.add_argument('--call-parameters', nargs='+', type=str)
-    parser.add_argument('--legend', action='store_true', dest='legend')
-    parser.add_argument('--config', default=ConfigParser.default_config)
-    parser.add_argument('--synonyms', nargs='+', type=str)
-    parser.add_argument('--name', default=None, type=str)
+    parser = ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--files', nargs='+', required=True, type=str, help='List of files that graphs will be build for. Every such file will be marked red on resulring graph')
+    parser.add_argument('--include-graph', dest='include_graph', action='store_true', help='Whether to build the include graph')
+    parser.add_argument('--call-graph', dest='call_graph', action='store_true', help='Whether to build the call graph')
+    parser.add_argument('--yaml-root', default='', help='Root directory that is used for resolving relative includes')
+    parser.add_argument('--call-display', choices=['none', 'text', 'edge'], default='text', help='How to display information about calls in call graph')
+    parser.add_argument('--rank-dir', choices=['left-right', 'up-down'], default='left-right', help='How to display a graph. If left-right is selected, all edges will be drawn from left to right. If up-down is selected, all edges will be drawn from up to bottom')
+    parser.add_argument('--call-parameters', nargs='+', type=str, help='Call parameters to display on call edges')
+    parser.add_argument('--legend', action='store_true', dest='legend', help='Whether to draw a legend or not')
+    parser.add_argument('--config', default=ConfigParser.default_config, help='Path to file used as config. Every option from config is overriden if set from command line parameters')
+    parser.add_argument('--synonyms', nargs='+', type=str, help='Sets of synonyms that should be considered the same. Every synonym set should be inside curly braces {}, every synonym inside set should be separated with comma')
+    parser.add_argument('--name', default=None, type=str, help='Export name. Include graphs will contain _include suffix. Call graphs will contain _call suffix. If not set, export name will be derived from the first file passed to --files')
 
     parser.set_defaults(include_graph=False, call_graph=False, draw_legend=False, call_parameters=[], synonyms=[], call_order=False)
 
