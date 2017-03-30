2b0019aa6f013c3a678f7fe9d08d652b952a32bc
missingdays <rebovykin@gmail.com>
2017-03-30 13:56:26 +0300

    Add doc images

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate .rst files, ingoring test folder
-sphinx-apidoc -f -o docs/ job_visualization/ job_visualization/test/
+sphinx-apidoc -f -o doc/ job_visualization/ job_visualization/test/
 
-cd docs
+cd doc
 make html
