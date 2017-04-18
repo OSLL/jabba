27b430009c1619a9e23fa370c1d709457e9177a4
missingdays <rebovykin@gmail.com>
2017-04-18 18:02:15 +0300

    Prepare for publishing

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from distutils.core import setup
+
+setup(
+    name='jabba',
+    packages=['jabba'],
+    version='0.1',
+    description='Jenkins And Job Builder Analysis',
+    author='Missingdays',
+    author_email='rebovykin@gmail.com',
+    url='https://github.com/OSLL/jenkins_job_builder_visualization',
+    download_url='https://github.com/OSLL/jenkins_job_builder_visualization/arhive/0.1.tar.gz',
+    keywords=['jenkins', 'job buider', 'visualization'],
+    scripts=['bin/jabba'],
+    classifiers=[]
+)
+
+
